9e32f45d90b2d03904b43bd6d360dc850874580b
Bianca21mota <biancasilvadarochapitamota@gmail.com>
2025-10-21 20:18:12 -0300

    feat:backend estoque

---
+++ Controller/EstoqueController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Controller;
+require_once __DIR__ . "/../Model/Connection.php";
+require_once __DIR__ . "/../Model/Estoque.php";
+
+
+use Model\Estoque;
+use Exception;
+
+
+
+?>
