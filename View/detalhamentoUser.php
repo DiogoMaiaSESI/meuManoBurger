1f8dd2c2f486ff3a597d5ee52a85700d372d6238
DiogoMaiaSESI <diogomaia.sesiba@gmail.com>
2025-10-22 11:55:39 -0300

    Add header section to detalhamentoUser.php

---
+++ View/detalhamentoUser.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Página de detalhamento | Meumanoburger</title>
+        <link rel="stylesheet" href="templates/assets/css/detalhamentoUser.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            <div class="sandwich">
+                <figure>
+                    <img src="" alt="">
+                </figure>
+                <div class="options">
+                    <div class="option">
+                    </div>
+                </div>
+            </div>
+            <figure>
+                <img src="" alt="">
+            </figure>
+            <div class="headerButtons">
+                <figure>
+                    <img src="" alt="">
+                </figure>
+                <figure>
+                    <img src="" alt="">
+                </figure>
+            </div>
+        </header>
+    </body>
+</html>
