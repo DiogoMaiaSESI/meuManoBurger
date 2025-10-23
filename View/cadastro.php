48aeeb4ff561e69c49c87b9a2fbca5a0dfe5eab7
Bianca21mota <biancasilvadarochapitamota@gmail.com>
2025-10-23 10:45:11 -0300

    fix: Ajuste no css

--- View/cadastro.php
+++ View/cadastro.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,77 +8,72 @@
     <link rel="icon" href="../templates/assets/img/Logo.png">
     <title>Cadastro | MeuManoBurger</title>
 </head>
+
 <body>
     <main>
         <div class="container">
             <div class="imagem_login">
-                
-                    <img src="../templates/assets/img/imglogin-cadastro.png" alt="Uma mulher sentada em uma cadeira com um notebook no colo, com um celular na página de login atrás">
+                <img src="../templates/assets/img/imglogin-cadastro.png"
+                    alt="Uma mulher sentada em uma cadeira com um notebook no colo, com um celular na página de login atrás">
             </div>
-
-            <div class="form_cadastro">
-                <h1>Seja Bem-Vindo ao MeumanoBurger</h1>
-                <h2>Cadastre-se</h2>
-                <p>E aproveite nossos recursos</p>
-            
-            
-                <form>
-            <div class="inputs">
-
-            <div class="foto">
-              <label for="imagemPerfil">
-
-             <p class = "adfoto"> Adicionar Foto</p>
-              <figure class="foto-do-perfil">
-                <img id="previewImagem" src="../templates/assets/img/perfil.png" alt="Imagem do perfil" />
-             </figure>
-
-              </label>
-                 <input type="file" id="imagemPerfil" name="imagemPerfil" accept="image/*" style="display: none;">
-            </div>
-
-                    </div>
-                     <p>Nome</p>
+            <div class="formContainer">
+                <div class="form_cadastro">
+                    <h1>Seja Bem-Vindo ao MeumanoBurger</h1>
+                    <h2>Cadastre-se</h2>
+                    <p>E aproveite nossos recursos</p>
+                    <form>
+                        <div class="inputs">
+                            <div class="foto">
+                                <label for="imagemPerfil">
+                                    <p class="adfoto"> Adicionar Foto</p>
+                                    <figure class="foto-do-perfil">
+                                        <img id="previewImagem" src="../templates/assets/img/perfil.png"
+                                            alt="Imagem do perfil" />
+                                    </figure>
+                                </label>
+                                <input type="file" id="imagemPerfil" name="imagemPerfil" accept="image/*"
+                                    style="display: none;">
+                            </div>
+                        </div>
+    
+                        <p class="labelInput">Nome</p>
                         <input type="text" name="nome" id="nome" required>
-            
-
-                        <p>Email</p>
+    
+                        <p class="labelInput">Email</p>
                         <input type="email" name="email" id="email" required>
-                       
-                        
-                        <p>Senha</p>
+    
+                        <p class="labelInput">Senha</p>
                         <input type="password" name="password" id="password" required>
-                       
-
-                        <p>Confirmar Senha</p>
+    
+                        <p class="labelInput">Confirmar Senha</p>
                         <input type="password" name="confirm_password" id="confirm_password" required>
-                         <p class="msg_erro">As senhas não coincidem</p>
-                    </div>
-
-                    <div class="btn_cadastrar">
+    
+                        <p class="msg_erro">As senhas não coincidem</p>
+    
                         <button type="submit">Cadastrar</button>
-                    </div>
-                </form>
-
-                <div class="google_facebook">
-                    <figure class="google">
-                        <a href=""> <!--Link para autenticação com o Google-->  
-                            <img src="../templates/assets/img/google.png" alt="Logo do Google">
-                        </a>
-                    </figure>
-
-                    <figure class="facebook">
-                        <a href=""> <!--Link para autenticação com o Facebook-->
-                            <img src="../templates/assets/img/facebook.png" alt="Logo do Facebook">
-                        </a>
-                    </figure>
+                        
+                        <div class="google_facebook">
+                            <figure class="google">
+                                <a href=""> <!--Link para autenticação com o Google-->
+                                    <img src="../templates/assets/img/google.png" alt="Logo do Google">
+                                </a>
+                            </figure>
+    
+                            <figure class="facebook">
+                                <a href=""> <!--Link para autenticação com o Facebook-->
+                                    <img src="../templates/assets/img/facebook.png" alt="Logo do Facebook">
+                                </a>
+                            </figure>
+                        </div>
+                        <p class="login"> Já tem uma conta? <span>Login</span></p>
+                    </form>
                 </div>
-                <p class="login"> Já tem uma conta? <span>Login</span></p>
-                <footer></footer>
             </div>
         </div>
+        </div>
     </main>
-
+    <footer></footer>
     <script src="../templates/assets/js/cadastro.js"></script>
 </body>
+
 </html>
