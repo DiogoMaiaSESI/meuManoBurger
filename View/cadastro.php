7f33cd5f2549a9d17d0962d57f2aebe184b791b8
Bianca21mota <biancasilvadarochapitamota@gmail.com>
2025-10-23 11:31:51 -0300

    feat: inicio da estrutura

--- View/cadastro.php
+++ View/cadastro.php
@@ -6,32 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../templates/assets/css/cadastro.css">
     <link rel="icon" href="../templates/assets/img/Logo.png">
-    <title>Cadastro | MeuManoBurger</title>
+    <title>Cadastro de Produto | MeuManoBurger</title>
 </head>
 
 <body>
     <main>
         <div class="container">
-            <div class="imagem_login">
-                <img src="../templates/assets/img/imglogin-cadastro.png"
-                    alt="Uma mulher sentada em uma cadeira com um notebook no colo, com um celular na página de login atrás">
+            <div class="logo">
+                <img src="../templates/assets/img/Logo.png"
+                    alt="A logo do MeuManoBurger">
             </div>
             <div class="formContainer">
-                <div class="form_cadastro">
-                    <h1>Seja Bem-Vindo ao MeumanoBurger</h1>
-                    <h2>Cadastre-se</h2>
-                    <p>E aproveite nossos recursos</p>
+                <div class="form_cadastro_produto">
+                    <h1>Cadastre seu novo produto</h1>
                     <form>
                         <div class="inputs">
                             <div class="foto">
-                                <label for="imagemPerfil">
+                                <label for="imagemProduto">
                                     <p class="adfoto"> Adicionar Foto</p>
-                                    <figure class="foto-do-perfil">
-                                        <img id="previewImagem" src="../templates/assets/img/perfil.png"
-                                            alt="Imagem do perfil" />
+                                    <figure class="foto-do-produto">
+                                        <img id="previewImagem" src="../templates/assets/img/camera.png"
+                                            alt="Imagem de camera para adicionar foto do produto" />
                                     </figure>
                                 </label>
-                                <input type="file" id="imagemPerfil" name="imagemPerfil" accept="image/*"
+                                <input type="file" id="imagemProduto" name="imagemProduto" accept="image/*"
                                     style="display: none;">
                             </div>
                         </div>
@@ -39,41 +37,30 @@
                         <p class="labelInput">Nome</p>
                         <input type="text" name="nome" id="nome" required>
     
-                        <p class="labelInput">Email</p>
+                        <p class="labelInput">Tipo</p>
+                        <select name="" id=""></select>
                         <input type="email" name="email" id="email" required>
     
-                        <p class="labelInput">Senha</p>
-                        <input type="password" name="password" id="password" required>
+                        <p class="labelInput">Descrição</p>
+                        <input type="text" name="descricao" id="descricao" required>
     
-                        <p class="labelInput">Confirmar Senha</p>
-                        <input type="password" name="confirm_password" id="confirm_password" required>
+                        <p class="labelInput">Quantidade</p>
+                        <input type="number" name="quantidade" id="quantidade" required>
     
-                        <p class="msg_erro">As senhas não coincidem</p>
+                        <p class="labelInput">Preço</p>
+                        <input type=" number" name="preco" id="preco" required>
     
                         <button type="submit">Cadastrar</button>
                         
-                        <div class="google_facebook">
-                            <figure class="google">
-                                <a href=""> <!--Link para autenticação com o Google-->
-                                    <img src="../templates/assets/img/google.png" alt="Logo do Google">
-                                </a>
-                            </figure>
-    
-                            <figure class="facebook">
-                                <a href=""> <!--Link para autenticação com o Facebook-->
-                                    <img src="../templates/assets/img/facebook.png" alt="Logo do Facebook">
-                                </a>
-                            </figure>
+                     
                         </div>
-                        <p class="login"> Já tem uma conta? <span>Login</span></p>
+        
                     </form>
                 </div>
             </div>
-        </div>
-        </div>
     </main>
     <footer></footer>
-    <script src="../templates/assets/js/cadastro.js"></script>
+    <script src="../templates/assets/js/cadastro_produto.js"></script>
 </body>
 
 </html>
