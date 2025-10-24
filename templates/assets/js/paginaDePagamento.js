743f958fe9eadb955431b741d2429fcf499b9d96
DiogoMaiaSESI <diogomaia.sesiba@gmail.com>
2025-10-24 16:02:11 -0300

    fix: correção nos botões do header.

--- templates/assets/js/paginaDePagamento.js
+++ templates/assets/js/paginaDePagamento.js
@@ -4,14 +4,21 @@ const sombra = document.querySelector('.sombra')
 const back = document.querySelector('.back')
 const cartPage = document.querySelector('.cartPage')
 const profileButton = document.querySelector('.profileButton')
+const sandwich = document.querySelector('.sandwich')
 
 menu.addEventListener('click', () => {
     opcoes.classList.toggle('optionActive')
     sombra.classList.toggle('shadowActive')
+    if(sandwich.style.pointerEvents === 'none') {
+        sandwich.style.pointerEvents = 'all'
+    } else {
+        sandwich.style.pointerEvents = 'none'
+    }
 })
 sombra.addEventListener('click', () => {
     opcoes.classList.toggle('optionActive')
     sombra.classList.toggle('shadowActive')
+    sandwich.style.pointerEvents = 'none'
 })
 
 back.addEventListener('click', () => {
