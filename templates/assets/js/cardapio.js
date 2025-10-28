22a6ef7afe84a0ef39af87f22a830dd64fcf6d63
PedroEmanoel933 <pemanoel067@gmail.com>
2025-10-28 12:05:20 -0300

    feat: Estrutura da página com responsividade para tablet e celular

--- templates/assets/js/cardapio.js
+++ templates/assets/js/cardapio.js
@@ -19,6 +19,8 @@ const labelCafe = document.querySelector('#cafe_manha h2')
 const labelDoces = document.querySelector('#doces h2')
 const labelTap = document.querySelector('#Tapioca h2')
 const labelProm = document.querySelector('#promocoes h2')
+const promHeader = document.querySelector('.h1_promocoes')
+const cardaHeader = document.querySelector('.h1_cardapio')
 
 
 hambBtn.addEventListener('click', () => {
@@ -36,6 +38,8 @@ hambBtn.addEventListener('click', () => {
     labelDoces.style.borderBottom = 'none'
     labelTap.style.borderBottom = 'none'
     labelProm.style.borderBottom = 'none'
+    cardaHeader.style.borderBottom = '0.1rem solid #FFFFFF'
+    promHeader.style.borderBottom = 'none'
 })
 lancBtn.addEventListener('click', () => {
     hambSection.style.display = 'none'
@@ -52,6 +56,8 @@ lancBtn.addEventListener('click', () => {
     labelDoces.style.borderBottom = 'none'
     labelTap.style.borderBottom = 'none'
     labelProm.style.borderBottom = 'none'
+    cardaHeader.style.borderBottom = '0.1rem solid #FFFFFF'
+    promHeader.style.borderBottom = 'none'
 })
 bebBtn.addEventListener('click', () => {
     hambSection.style.display = 'none'
@@ -68,6 +74,8 @@ bebBtn.addEventListener('click', () => {
     labelDoces.style.borderBottom = 'none'
     labelTap.style.borderBottom = 'none'
     labelProm.style.borderBottom = 'none'
+    cardaHeader.style.borderBottom = '0.1rem solid #FFFFFF'
+    promHeader.style.borderBottom = 'none'
 })
 cafeBtn.addEventListener('click', () => {
     hambSection.style.display = 'none'
@@ -84,6 +92,8 @@ cafeBtn.addEventListener('click', () => {
     labelDoces.style.borderBottom = 'none'
     labelTap.style.borderBottom = 'none'
     labelProm.style.borderBottom = 'none'
+    cardaHeader.style.borderBottom = '0.1rem solid #FFFFFF'
+    promHeader.style.borderBottom = 'none'
 })
 docesBtn.addEventListener('click', () => {
     hambSection.style.display = 'none'
@@ -100,6 +110,8 @@ docesBtn.addEventListener('click', () => {
     labelDoces.style.borderBottom = '2px solid #ffcc00'
     labelTap.style.borderBottom = 'none'
     labelProm.style.borderBottom = 'none'
+    cardaHeader.style.borderBottom = '0.1rem solid #FFFFFF'
+    promHeader.style.borderBottom = 'none'
 })
 tapBtn.addEventListener('click', () => {
     hambSection.style.display = 'none'
@@ -116,6 +128,8 @@ tapBtn.addEventListener('click', () => {
     labelDoces.style.borderBottom = 'none'
     labelTap.style.borderBottom = '2px solid #ffcc00'
     labelProm.style.borderBottom = 'none'
+    cardaHeader.style.borderBottom = '0.1rem solid #FFFFFF'
+    promHeader.style.borderBottom = 'none'
 })
 promBtn.addEventListener('click', () => {
     hambSection.style.display = 'none'
@@ -132,4 +146,49 @@ promBtn.addEventListener('click', () => {
     labelDoces.style.borderBottom = 'none'
     labelTap.style.borderBottom = 'none'
     labelProm.style.borderBottom = '2px solid #ffcc00'
+    promHeader.style.borderBottom = '0.1rem solid #FFFFFF'
+    cardaHeader.style.borderBottom = 'none'
 })
+
+if (promHeader) {
+    promHeader.addEventListener('click', () => {
+        hambSection.style.display = 'none'
+        lancSection.style.display = 'none'
+        bebSection.style.display = 'none'
+        cafeSection.style.display = 'none'
+        docesSection.style.display = 'none'
+        tapSection.style.display = 'none'
+        promSection.style.display = 'block'
+        labelHamb.style.borderBottom = 'none'
+        labelLanc.style.borderBottom = 'none'
+        labelBeb.style.borderBottom = 'none'
+        labelCafe.style.borderBottom = 'none'
+        labelDoces.style.borderBottom = 'none'
+        labelTap.style.borderBottom = 'none'
+        labelProm.style.borderBottom = '2px solid #ffcc00'
+        promHeader.style.borderBottom = '0.1rem solid #FFFFFF'
+        cardaHeader.style.borderBottom = 'none'
+    })
+}
+
+
+if (cardaHeader) {
+    cardaHeader.addEventListener('click', () => {
+        hambSection.style.display = 'block'
+        lancSection.style.display = 'none'
+        bebSection.style.display = 'none'
+        cafeSection.style.display = 'none'
+        docesSection.style.display = 'none'
+        tapSection.style.display = 'none'
+        promSection.style.display = 'none'
+        labelHamb.style.borderBottom = '2px solid #ffcc00'
+        labelLanc.style.borderBottom = 'none'
+        labelBeb.style.borderBottom = 'none'
+        labelCafe.style.borderBottom = 'none'
+        labelDoces.style.borderBottom = 'none'
+        labelTap.style.borderBottom = 'none'
+        labelProm.style.borderBottom = 'none'
+        promHeader.style.borderBottom = 'none'
+        cardaHeader.style.borderBottom = '0.1rem solid #FFFFFF'
+    })
+}
