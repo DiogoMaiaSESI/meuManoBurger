189ca3d397e339b432a1de370cb68f8d1d13c41c
PedroEmanoel933 <pemanoel067@gmail.com>
2025-10-29 10:47:05 -0300

    feat: Adição do menu sanduíche na página

--- templates/assets/js/cardapio.js
+++ templates/assets/js/cardapio.js
@@ -192,3 +192,19 @@ if (cardaHeader) {
         cardaHeader.style.borderBottom = '0.1rem solid #FFFFFF'
     })
 }
+
+
+const opcoes = document.querySelector('.options')
+const menu = document.querySelector('.menu')
+const sombra = document.querySelector('.sombra')
+const sandwich = document.querySelector('.sandwich')
+menu.addEventListener('click', () => {
+    opcoes.classList.toggle('optionActive')
+    sombra.classList.toggle('shadowActive')
+    sandwich.classList.toggle('sandwichActive') 
+})
+sombra.addEventListener('click', () => {
+    opcoes.classList.toggle('optionActive')
+    sombra.classList.toggle('shadowActive')
+    sandwich.classList.toggle('sandwichActive')
+})
