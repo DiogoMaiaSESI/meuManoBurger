801661fafe946ba7a43b8cb74cf2cd83046deb7f
biamot4 <beatrizmotasesi@gmail.com>
2025-10-29 12:18:07 -0300

    Feat: Adição do Modal de mais detalhes

--- templates/assets/js/pedidosADM.js
+++ templates/assets/js/pedidosADM.js
@@ -1,6 +1,11 @@
 const opcoes = document.querySelector('.options')
 const menu = document.querySelector('.menu')
 const sombra = document.querySelector('.sombra')
+const btndetalhes = document.querySelector('.detalhes')
+const btnstatusbtn = document.querySelector('.statusbtn')
+const modaldetalhes = document.querySelector('.modaldetalhes')
+const fechar = document.querySelector('.fechar')
+
 menu.addEventListener('click', () => {
 opcoes.classList.toggle('optionActive')
 sombra.classList.toggle('shadowActive')
@@ -8,4 +13,18 @@ sombra.classList.toggle('shadowActive')
 sombra.addEventListener('click', () => {
 opcoes.classList.toggle('optionActive')
 sombra.classList.toggle('shadowActive')
+})
+
+btndetalhes.addEventListener('click', () => {
+    modaldetalhes.style.display = "block"
+})
+
+fechar.addEventListener('click', () => {
+    modaldetalhes.style.display = "none"
+})
+
+window.addEventListener('click', (event) => {
+  if (event.target == modaldetalhes) {
+    modaldetalhes.style.display = "none";
+  }
 })
