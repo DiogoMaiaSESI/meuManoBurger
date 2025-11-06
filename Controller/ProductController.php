a1645f207489c3d10f33b2b4033a99a50813758a
DiogoMaiaSESI <diogomaia.sesiba@gmail.com>
2025-11-06 12:28:02 -0300

    fix: Remoção da sanitização de dados.

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -12,24 +12,6 @@ class ProductController {
     }
 
     public function create($nome, $preco, $tipo, $descricao, $imagem, $id_adm_fk) {
-        if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-            return ['success' => false, 'errors' => ['Requisição inválida.']];
-        }
-
-        $nome = filter_input(INPUT_POST, 'nome_produto', FILTER_SANITIZE_SPECIAL_CHARS);
-        $preco = filter_input(INPUT_POST, 'preco_produto', FILTER_VALIDATE_FLOAT);
-        $tipo = filter_input(INPUT_POST, 'tipo_produto', FILTER_SANITIZE_SPECIAL_CHARS);
-        $descricao = filter_input(INPUT_POST, 'descricao_produto', FILTER_SANITIZE_SPECIAL_CHARS);
-        $id_adm_fk = filter_input(INPUT_POST, 'id_adm_fk', FILTER_VALIDATE_INT);
-
-        $imagem = null;
-        if (isset($_FILES['imagem_produto']) && $_FILES['imagem_produto']['error'] == UPLOAD_ERR_OK) {
-            $fileType = mime_content_type($_FILES['imagem_produto']['tmp_name']);
-            if (in_array($fileType, ['image/jpeg', 'image/png', 'image/gif'])) {
-                 $imagem = file_get_contents($_FILES['imagem_produto']['tmp_name']);
-            }
-        }
-
         return $this->productModel->createProduct($nome, $preco, $tipo, $descricao, $imagem, $id_adm_fk);
     }
 
