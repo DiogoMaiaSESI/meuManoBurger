f4e889049ee5beb64c33431d365e50753c42b639
DiogoMaiaSESI <diogomaia.sesiba@gmail.com>
2025-11-24 16:11:42 -0300

    style: Correção no tamanho dos feedbacks da página inicial.

--- Config/configuration.php
+++ Config/configuration.php
@@ -1,7 +1,13 @@
 <?php
+// define("DB_NAME","meumanoburger");
+// define("DB_HOST","turntable.proxy.rlwy.net");
+// define("DB_USER","root");
+// define("DB_PASSWORD",'EJZmRSRXurjvfASLBYfIqErbVNYgMpiX');
+// define("DB_PORT","10005");
+
 define("DB_NAME","meumanoburger");
-define("DB_HOST","turntable.proxy.rlwy.net");
-define("DB_USER","root");
-define("DB_PASSWORD",'EJZmRSRXurjvfASLBYfIqErbVNYgMpiX');
-define("DB_PORT","10005");
+define("DB_HOST","127.0.0.1");
+define("DB_USER","diogo");
+define("DB_PASSWORD",'Li2805ux!');
+define("DB_PORT","3306");
 ?>
