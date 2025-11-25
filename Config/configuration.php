64284e0031b2e378ff35c2ceaa4a2ce8ac475538
DiogoMaiaSESI <diogomaia.sesiba@gmail.com>
2025-11-24 21:09:41 -0300

    fix: Atualização do configuration para banco de dados real.

--- Config/configuration.php
+++ Config/configuration.php
@@ -1,15 +1,15 @@
 <?php
-// define("DB_NAME","meumanoburger");
-// define("DB_HOST","turntable.proxy.rlwy.net");
-// define("DB_USER","root");
-// define("DB_PASSWORD",'EJZmRSRXurjvfASLBYfIqErbVNYgMpiX');
-// define("DB_PORT","10005");
-
 define("DB_NAME","meumanoburger");
-define("DB_HOST","localhost");
+define("DB_HOST","turntable.proxy.rlwy.net");
 define("DB_USER","root");
-define("DB_PASSWORD",'');
-define("DB_PORT","3306");
+define("DB_PASSWORD",'EJZmRSRXurjvfASLBYfIqErbVNYgMpiX');
+define("DB_PORT","10005");
+
+// define("DB_NAME","meumanoburger");
+// define("DB_HOST","localhost");
+// define("DB_USER","root");
+// define("DB_PASSWORD",'');
+// define("DB_PORT","3306");
 
 // define("DB_NAME","meumanoburger");
 // define("DB_HOST","127.0.0.1");
